f9629bc10a6b6907cef69b6938d8355f0901fbfe
Biagio <biagior00@gmail.com>
2023-04-12 20:35:11 +0200

    Initial Commit

---
+++ MinimalisticWeather/MinimalisticWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalisticWeatherApp.swift
+//  MinimalisticWeather
+//
+//  Created by Biagio Ricci on 12/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MinimalisticWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
